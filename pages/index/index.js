d8424b650b53b3691261b1f46a87f32d0a49859c
vaanxy <274824370@qq.com>
2018-06-07 23:48:39 +0800

    完成新闻小程序所有基本功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,6 @@
 //index.js
+
+// 所有新闻类别标签的数据
 const categories = [
   { code: 'gn', name: '国内' },
   { code: 'gj', name: '国际' },
@@ -7,11 +9,12 @@ const categories = [
   { code: 'js', name: '军事' },
   { code: 'ty', name: '体育' },
   { code: 'other', name: '其他' }
-]
+];
+
 Page({
   data: {
     categories: categories,
-    selectedCategory: categories[0],
+    selectedCategory: categories[0], //当前选中的新闻类别，默认选中第一个
     newsList: [],
     hotNews: {
       title: '加载中...',
@@ -20,9 +23,18 @@ Page({
       firstImage: '/images/cloudy-bg.png'
     }
   },
+
+  /**
+   * 页面加载后，通过getNewsList函数从网络获取当前选中类别的新闻列表
+   */
   onLoad() {
     this.getNewsList();
   },
+
+  /**
+   * 点击新闻类别选项卡时，切换新闻类别选项卡，
+   * 并重新调用onTapCategory函数获取新选中类别的新闻列表数据
+   */
   onTapCategory(event) {
     console.log(event);
     let category = event.currentTarget.dataset['category'];
@@ -31,9 +43,19 @@ Page({
     });
     this.getNewsList();
   },
+
+  /**
+   * 下拉刷新，重新调用getNewsList函数加载当前类别的新闻列表信息，
+   * 并需要传入回调函数，在网络请求完成后(无论成功/失败)停止下拉刷新
+   */
   onPullDownRefresh() {
     this.getNewsList(() => wx.stopPullDownRefresh());
   },
+
+  /**
+   * @param: cb 回调函数, 
+   * 若传入该函数参数，则会在网络获取数据完成后(无论成功/失败)执行,
+   */
   getNewsList(cb) {
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
@@ -43,6 +65,7 @@ Page({
       success: (res) => {
         console.log(res.data.result);
         let newsList = [];
+        // 构造用于显示的新闻列表
         res.data.result.forEach(news => {
           let date = new Date(news.date);
           newsList.push({
@@ -61,13 +84,17 @@ Page({
       complete: () => {
         cb && cb();
       }
-    })
+    });
   },
+
+  /**
+   * 跳转到对应newsId的新闻详情页面
+   */
   toNewsDetail(event) {
     let newsId = event.currentTarget.dataset.id;
     console.log(newsId);
     wx.navigateTo({
       url: '/pages/news-detail/news-detail?newsId=' + newsId,
-    })
+    });
   }
 });
--- pages/news-detail/news-detail.js
+++ pages/news-detail/news-detail.js
@@ -22,19 +22,45 @@ Page({
         newsId: newsId
       });
     }
+    this.getNewsDetail();
 
   },
 
   /**
    * 下拉刷新，调用getNewsDetail函数重新取新闻详情信息
    */
-  onPullDownRefresh: () => {
-  
+  onPullDownRefresh() {
+    this.getNewsDetail(() => wx.stopPullDownRefresh());
   },
+
   /**
    * 调用接口, 根据newsId获取新闻详情信息
    */
-  getNewsDetail() {
-    
+  getNewsDetail(cb) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: this.data.newsId
+      },
+      success: (res) => {
+        let news = res.data.result;
+        console.log(news);
+        let date = new Date(news.date);
+        this.setData({
+          newsDetail: {
+            id: news.id,
+            title: news.title,
+            date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`,
+            source: news.source === '' ? '未知来源' : news.source,
+            readCount: news.readCount,
+            firstImage: news.firstImage === '' ? '/images/sunny-bg.png' : news.firstImage,
+            content: news.content
+          }
+        });
+      },
+      complete: () => {
+        cb && cb();
+      }
+    });
   }
 })
